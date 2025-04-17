ed656de864266c57a0870bc6da012de5a8da007c
Ns <fwork761@gmail.com>
2025-04-17 17:49:58 +1000

    Реализована фабрика для генерации подключений к разным БД

--- factory_connect/fact_conn.py
+++ factory_connect/fact_conn.py
@@ -10,14 +10,21 @@ class FactoryConnect:
         pass
 
     @classmethod
-    def register_db_connector(cls, conn: list):
-        key = cls._get_instance_key(*conn)
-        if key is not cls._DB_CONNECTIONS:
-            cls._DB_CONNECTIONS[conn] = py_db_helper.Base()
+    def register_db_connector(cls, conn: dict):
+        key = cls._get_instance_key(conn)
+        if key not in cls._DB_CONNECTIONS:
+            cls._DB_CONNECTIONS[key] = py_db_helper.Base(**conn)
+    
+    @classmethod
+    def get_db_conn_obj(cls, conn: dict):
+        key = cls._get_instance_key(conn)
+        if key not in cls._DB_CONNECTIONS:
+            return None
+        return cls._DB_CONNECTIONS[key]
 
     @classmethod
-    def _get_instance_key(cls, *args):
-        return str(args)
+    def _get_instance_key(cls, args: dict):
+        return str(list(args.values())[0:-1:1])
 
     @classmethod
     def add_supported_driver(cls, driver_name: str):
@@ -25,7 +32,17 @@ class FactoryConnect:
         cls.supported_drivers.add(driver_name.lower())
 
     @classmethod
-    def _check_param(cls, driver_name, username, password, host, database, port, *args):
+    def delate_supported_driver(cls, driver_name: str):
+        """Добавить новый поддерживаемый драйвер"""
+        cls.supported_drivers.remove(driver_name.lower())
+
+    @classmethod
+    def get_number_of_connections(cls):
+        return len(cls._DB_CONNECTIONS)
+
+    @classmethod
+    def _check_param(cls, driver_name, username, password,
+                     host, database, port, *args):
 
         if not isinstance(driver_name, str) or driver_name.lower() not in cls.supported_drivers:
             raise ValueError(f"Поддерживаемые драйверы: {cls.supported_drivers}. Получено: {driver_name}")
@@ -42,5 +59,5 @@ class FactoryConnect:
         if not isinstance(database, str) or not database:
             raise ValueError("Название базы данных должно быть строкой и не пустым.")
 
-        if not isinstance(port, (str, int)) or not str(port).isdigit() or not (1 <= int(port) <= 65535):
+        if not isinstance(port, (int)) or not (1 <= int(port) <= 65535):
             raise ValueError(f"Некорректный порт: {port}")
--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from db import py_db_helper
+from dataclasses import dataclass
 from factory_connect import fact_conn
+
 
 def main():
 
     fc = fact_conn.FactoryConnect()
-    conn = ["postgresql", "postgres", "postgress", "localhost", "usad", "5400"  ,
-            ["postgresql", "postgres", "postgress", "localhost", "usad", "5401"],
-            ["postgresql", "postgres", "postgress", "localhost", "usad", "5402"],
-            ["postgresql", "postgres", "postgress", "localhost", "usad", "5403"]]
+    conn = [{'driver_name': 'postgresql', 'username': 'postgres',
+             'password': 'postgress', 'host': 'localhost',
+             'database': 'Test', 'port': '5400', 'auto_connect': True},
+             {'driver_name': 'postgresql', 'username': 'postgres',
+             'password': 'postgress', 'host': 'localhost',
+             'database': 'usad', 'port': '5400', 'auto_connect': False}]
     
-    # base = py_db_helper.Base("postgresql", username="postgres", password="postgress",
-    #             host="localhost", database="usad", port="5400", auto_connect=False)
 
-    # base2 = py_db_helper.Base("postgresql", username="postgres", password="postgress",
-    #              host="localhost", database="usad", port="5400", auto_connect=False)
+    for i in conn:
+        fc.register_db_connector(i)
+    
+    print(f'Количество подключений: {fc.get_number_of_connections()}')
+    print(f'Объекты подключения: {fc.get_db_conn_obj(conn[0])}')
 
-    # base3 = py_db_helper.Base("postgresql", username="postgres", password="postgress",
-    #              host="localhost", database="usad", port="5401", auto_connect=False)
+    conn2 = {'driver_name': 'postgresql', 'username': 'postgres',
+             'password': 'postgress', 'host': 'localhost',
+             'database': 'usad', 'port': '5401', 'auto_connect': False}
+    fc.register_db_connector(conn2)
+    print(f'Количество подключений: {fc.get_number_of_connections()}')
+    
+    obj = fc.get_db_conn_obj(conn[0])
+    print(obj.sql_read('select * from test_part;', 'partition'))
 
-    # print(f'Проверяем, является ли объект base и base2 одинаковыми: {base is base2}')
-    # print(f'Проверяем, является ли объект base и base3 одинаковыми: {base is base3}')
-    # print(f'Проверяем, является ли объект base2 и base3 одинаковыми: {base3 is base2}')
 
 
 if __name__ == '__main__':
